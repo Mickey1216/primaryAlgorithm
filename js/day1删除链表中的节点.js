56e3ae8c9b3e5c4438ab3baa81899d48cff48e78
liululu <383449174@qq.com>
2021-11-09 21:26:15 +0800

    day1

---
+++ js/day1删除链表中的节点.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问 要被删除的节点 。
+题目数据保证需要删除的节点 不是末尾节点 。
+
+示例：
+输入：head = [4,5,1,9], node = 5
+输出：[4,1,9]
+解释：指定链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9
+
+提示：
+链表中节点的数目范围是 [2, 1000]
+-1000 <= Node.val <= 1000
+链表中每个节点的值都是唯一的
+需要删除的节点 node 是 链表中的一个有效节点 ，且 不是末尾节点
+ */
+//Definition for singly-linked list.
+var ListNode = /** @class */ (function () {
+    function ListNode(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+    return ListNode;
+}());
+//Do not return anything, modify it in-place instead.
+function deleteNode(root) {
+}
+;
