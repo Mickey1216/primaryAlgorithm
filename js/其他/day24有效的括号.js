9e8827644fdd604cdb238da8f169a286f448b31a
liululu <383449174@qq.com>
2021-11-18 15:40:09 +0800

    finish other

--- js/其他/day24有效的括号.js
+++ js/其他/day24有效的括号.js
@@ -1,1 +1,55 @@
 "use strict";
+/**
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
+有效字符串需满足：
+    左括号必须用相同类型的右括号闭合。
+    左括号必须以正确的顺序闭合。
+ 
+示例 1：
+输入：s = "()"
+输出：true
+
+示例 2：
+输入：s = "()[]{}"
+输出：true
+
+示例 3：
+输入：s = "(]"
+输出：false
+
+示例 4：
+输入：s = "([)]"
+输出：false
+
+示例 5：
+输入：s = "{[]}"
+输出：true
+ */
+function isValid(s) {
+    //利用栈解决
+    const res = [];
+    const left = ['(', '[', '{']; //左括号
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+    //左括号入栈，右括号出栈
+    for (let i = 0; i < s.length; i++) {
+        let item = s[i];
+        if (left.includes(item)) {
+            res.push(item);
+        }
+        else {
+            let top = res[res.length - 1];
+            if (top === map[item]) {
+                res.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return res.length === 0;
+}
+console.log(isValid("()[]{}"));
---
+++ 其他/day22汉明距离.ts
@@ -0,0 +1,56 @@
+/**
+两个整数之间的 汉明距离 指的是这两个数字对应二进制位不同的位置的数目。
+给你两个整数 x 和 y，计算并返回它们之间的汉明距离。
+
+示例 1：
+输入：x = 1, y = 4
+输出：2
+解释：
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+上面的箭头指出了对应二进制位不同的位置。
+
+示例 2：
+输入：x = 3, y = 1
+输出：1
+ */
+
+function hammingDistance(x: number, y: number): number {
+       //x和y的二进制
+       let bx = x.toString(2).split('')
+       let by = y.toString(2).split('')
+
+       //填充0
+       while (bx.length % 4 !== 0) {
+              bx.unshift('0')
+       }
+       while (by.length % 4 !== 0) {
+              by.unshift('0')
+       }
+
+       //让bx和by的长度相同
+       if (bx.length < by.length) {
+              let diff = by.length - bx.length
+              for (let i = 0; i < diff; i++) {
+                     bx.unshift('0')
+              }
+       } else {
+              let diff = bx.length - by.length
+              for (let i = 0; i < diff; i++) {
+                     by.unshift('0')
+              }
+       }
+
+       //比较bx和by的每一位
+       let count = 0
+       for(let i=0;i<bx.length;i++){
+              if(bx[i] !== by[i])
+                     count++
+       }
+      
+       return count
+}
+
+console.log(hammingDistance(3, 1))
+
